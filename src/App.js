4fe39c625a439415f2454a237c8430b5ba831481
Minh Nguyen <minhnguyen.dev.98@gmail.com>
2021-12-07 23:01:56 +0700

    feat: init Monsters Project

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import './App.css';
+import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component'
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchField: '',
+    }
+  }
+
+  componentDidMount = () => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }))
+  }
+
+  handleOnChange = (e) => {
+    this.setState({searchField: e.target.value})
+  }
+
+  render() {
+    const { monsters, searchField } = this.state
+    const filteredMonster = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLocaleLowerCase()))
+    return (
+      <div className="App">
+        <SearchBox
+          placeholder='search monsters'
+          handleOnChange={this.handleOnChange}
+        />
+        <CardList monsters={filteredMonster} />
+      </div>
+    );
+  }
+
+}
+
+export default App;
---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./search-box.styles.css";
+
+export const SearchBox = ({ placeholder, handleOnChange }) => {
+    return  <input
+    className="search"
+    type="search"
+    placeholder={placeholder}
+    onChange={handleOnChange}
+  />
+}
